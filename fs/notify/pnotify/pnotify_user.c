6560add7226febdcb8d74d2509447ff38318013a
Konstantin Bukin <kbukin@nvidia.com>
2015-10-24 10:59:13 -0700

    1. "make menuconfig" sets CONFIG_PNOTIFY_USER define now     2. added placeholders for pnotify_user.c and pnotify_fsnotify.c

---
+++ fs/notify/pnotify/pnotify_user.c
@@ -0,0 +1,29 @@
+/*
+ * pnotify_user.c - inotify support for userspace
+ *
+ * Authors:
+ *	John McCutchan	<ttb@tentacle.dhs.org>
+ *	Robert Love	<rml@novell.com>
+ *
+ * Copyright (C) 2005 John McCutchan
+ * Copyright 2006 Hewlett-Packard Development Company, L.P.
+ *
+ * Copyright (C) 2009 Eric Paris <Red Hat Inc>
+ * inotify was largely rewriten to make use of the fsnotify infrastructure
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ *  This is pnotify_user.c, which is a copy and modification of inotify_user.c
+ *
+ *  --John F. Hubbard <jhubbard@nvidia.com> 01 Dec 2011
+ *
+ */
+
