c78d65df3f28722848856e44c1f1e7a87fea3336
Konstantin Bukin <kbukin@nvidia.com>
2015-10-24 11:02:33 -0700

    added pnotify data members to task_struct and user_struct

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -819,6 +819,10 @@ struct user_struct {
 	atomic_t inotify_watches; /* How many inotify watches does this user have? */
 	atomic_t inotify_devs;	/* How many inotify devs does this user have opened? */
 #endif
+#ifdef CONFIG_PNOTIFY_USER
+     atomic_t pnotify_watches; /* How many inotify watches does this user have? */
+     atomic_t pnotify_devs;  /* How many inotify devs does this user have opened? */
+#endif
 #ifdef CONFIG_FANOTIFY
 	atomic_t fanotify_listeners;
 #endif
@@ -1408,6 +1412,10 @@ struct task_struct {
 #ifdef CONFIG_PREEMPT_NOTIFIERS
 	/* list of struct preempt_notifier: */
 	struct hlist_head preempt_notifiers;
+#endif
+#ifdef CONFIG_PNOTIFY_USER
+     u32 pnotify_mask; /* all events this task cares about */
+     struct hlist_head pnotify_marks;
 #endif
 
 #ifdef CONFIG_BLK_DEV_IO_TRACE
