b8ba40f5c2ed3b4255aae0cf55945f84523e1a05
Konstantin Bukin <kbukin@nvidia.com>
2015-10-25 09:26:29 -0700

    wired up pnotify to copy_files() fixed printf in pnotify_fullpath_from_path() to check for null pointer added init of pnotify marks passing status to fsnotify_access/fsnotify_modify from corresponding vfs.c functions passing extra NULL (for *task) to fsnotify_add_mark_locked() in fanotify_add_new_mark some 0/NULL cleanup

--- fs/notify/inode_mark.c
+++ fs/notify/inode_mark.c
@@ -235,9 +235,9 @@ void fsnotify_unmount_inodes(struct list_head *list)
 			iput(need_iput_tmp);
 
 		/* for each watch, send FS_UNMOUNT and then remove it */
-		fsnotify(inode, FS_UNMOUNT, inode, FSNOTIFY_EVENT_INODE, NULL, 0, 0, 0);
+		fsnotify(inode, FS_UNMOUNT, inode, FSNOTIFY_EVENT_INODE, NULL, 0, NULL, 0);
 
-		fsnotify_inode_delete(inode, 0);
+		fsnotify_inode_delete(inode, NULL);
 
 		iput(inode);
 
