0e70cd550f750b32e91a01569e99f07e74078b5a
Konstantin Bukin <kbukin@nvidia.com>
2016-01-08 09:43:09 -0800

    bumped up version to 1.20

--- fs/notify/pnotify/pnotify_user.c
+++ fs/notify/pnotify/pnotify_user.c
@@ -1310,7 +1310,7 @@ static int __init pnotify_user_setup(void)
 
 	pnotify_debug_print_level = 0;
 	pnotify_major_version = 1;
-	pnotify_minor_version = 19;
+	pnotify_minor_version = 20;
 
 	return 0;
 }
